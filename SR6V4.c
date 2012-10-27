c3365e0a746c0d8ad5f7f62ba96f74de6e2e6fc8
Yaskovich Dmitry <kadavr95@outlook.com>
2012-10-27 14:02:00 +0300

    initial commit

---
+++ SR6V4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <limits.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(void){
+  int m[12],av,sum=0,a,i,n=0;
+  printf("Exercise 6. Variant 4\n\n");
+  for (i = ; i < 10; i++) {
+
+  }
+  printf("Dimini Inc.\n");
+  printf("For progressive future\n");
+  printf("(c)2009-2012\n");
+  fflush(stdin);
+  getchar();
+  return 0;
+}
