48b4b28706705acd7703595eb04cee2ea0581382
Yaskovich Dmitry <kadavr95@outlook.com>
2012-10-28 01:37:00 +0300

    -

--- SR6V4.c
+++ SR6V4.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
-#include <limits.h>
 #include <math.h>
-#include <stdlib.h>
 
 int main(void){
-  int m[12],av,sum=0,a,i,n=0;
-  printf("Exercise 6. Variant 4\n\n");
-  for (i = 0; i <=11; i++) {
-
+  float m[29], min=6,av,sum=0;
+  short int x=-14;
+  printf("Yaskovich Dmitry (T01-01c). Lab 6. Variant 22\n\n");
+  for (x = -14; x <= 14; x+=0,7) {
+	m[x+14]=5*cos(0.4*x);
+	if (m[x+14]<min) {
+	  min=m[x+14];
+	}
+	printf("No:%2d Element: %13.10f\n",x+14,m[x+14]);
   }
+  for (x = 0; x <= 28; x+=2) {
+	sum+=m[x];
+  }
+  av=sum/29;
+  printf("\nMinimum:       %13.10f\n",min);
+  printf("Average:       %13.10f\n\n",av);
   printf("Dimini Inc.\n");
   printf("For progressive future\n");
   printf("(c)2009-2012\n");
