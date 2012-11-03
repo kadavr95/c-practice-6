dc707c3faa4f5c8bcbe76b81e6819dc2734c83e1
Yaskovich Dmitry <kadavr95@outlook.com>
2012-11-03 12:23:00 +0300

    added calculation of the average

--- SR6V4.c
+++ SR6V4.c
@@ -6,8 +6,8 @@
 const int k = 13;
 
 int main(void){
-  int d,dd,a,n,e[13],i,j,t;                                               //определение переменных и присваивание им необходимых значений
-  float m;
+  int d,dd,a,n,e[13],i,j,t,sum=0,kv=0;                                               //определение переменных и присваивание им необходимых значений
+  float m,av;
   int stime;
   long int ltime;
   printf("Yaskovich Dmitry (T01-01c). Exercise 6. Variant 4\n\n");                  //Информация о программе
@@ -18,6 +18,10 @@ int main(void){
   srand(stime);
   for (i = 1; i <= (k-1); i++) {
 	e[i]=d+(int)((dd-d)*(rand() / (RAND_MAX + 1.0)));
+	if (e[i]<a) {
+	 sum+=e[i];
+	 kv+=1;
+	}
   }
   if (((k-1)%6)==0)
 	t=k/6;
@@ -37,6 +41,12 @@ int main(void){
 	printf("\n");
   }
   printf("-------------------------------------------\n");
+  if (kv>0) {
+  av=sum/kv;
+  printf("Average of elements less than A: %f\n",av);
+  }
+  else
+  printf("Average of elements less than A: N/A\n");
   printf("Dimini Inc.\n");                                                      //Авторские права
   printf("For progressive future\n");
   printf("(c)2009-2012\n");
